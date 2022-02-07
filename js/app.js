90a5d8ad5fe1938f83a87c0ba00300c92e05b941
vladimiraurelius <vladimiraurelius@gmail.com>
2022-02-07 11:38:58 +0200

    Test_2

--- js/app.js
+++ js/app.js
@@ -2,6 +2,7 @@ document.addEventListener('DOMContentLoaded', () => {
 
     const links = document.querySelectorAll('.header__list-link');
     const linksParent = document.querySelector('.header__list');
+    const buttonOrder = document.querySelector('.about__button');
 
     function onActiveLink(i=0) {
         links.forEach((item) => item.classList.remove('header__list-link--active'));
@@ -38,6 +39,12 @@ document.addEventListener('DOMContentLoaded', () => {
             });
         }
         
+    });
+
+    buttonOrder.addEventListener('click', (e) => {
+        e.preventDefault();
+        onActiveLink(3);
+        scrollToPlace();
     });
     
     onActiveLink();

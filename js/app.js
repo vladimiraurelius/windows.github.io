744d3e8def9eddff868c37e810ce251d3deb2cf1
vladimiraurelius <vladimiraurelius@gmail.com>
2022-02-16 17:49:26 +0200

    add_popup_window

--- js/app.js
+++ js/app.js
@@ -1,5 +1,5 @@
 document.addEventListener('DOMContentLoaded', () => {
-
+// Прокрутка страницы
     const links = document.querySelectorAll('.header__list-link');
     const linksParent = document.querySelector('.header__list');
     const buttonOrder = document.querySelector('.about__button');
@@ -50,4 +50,63 @@ document.addEventListener('DOMContentLoaded', () => {
     onActiveLink();
     scrollToPlace();
 
+// Управление модальным окном
+
+    const popupButtons = document.querySelectorAll('.service__eye');
+    const popupButtonsParent = document.querySelector('.services__items');
+    const popup = document.querySelector('.popup');
+    const popupContent = document.querySelector('.popup__content')
+    const container = document.querySelector('.container');
+
+    popupButtonsParent.addEventListener('click', (event) => {
+        const target = event.target;
+
+        if(target && target.classList.contains('service__eye')) {
+            popupButtons.forEach((item) => {
+                if (target == item){
+                    if(item.dataset.length != 0){
+                        openPopup();
+
+                        let closeButton = popup.querySelector('.popup__close');
+                        closeButton.addEventListener('click', closePopup);
+
+                        createPopupContent(item); 
+                    }                                      
+                    
+                }
+            })
+        }
+
+    });
+
+    function openPopup() {
+        popup.classList.add('popup--active');
+        popup.scrollIntoView();
+
+        container.classList.add('container--blocked');        
+    }
+
+    function closePopup() {
+        popup.querySelectorAll('.popup__img').forEach(img => img.remove());
+        popup.classList.remove('popup--active');        
+
+        container.classList.remove('container--blocked');
+    }
+
+    function createPopupContent(item) {
+        let imageParent = item.dataset.parent;
+        let imageLength = item.dataset.length;
+        
+
+        for(let i=1; i<imageLength; i++){
+            let popupPhoto = document.createElement('img');
+            popupPhoto.classList.add('popup__img');
+            popupPhoto.alt = 'photo';
+            popupPhoto.src = `./img/service/${imageParent}/${i}.jpg`;
+
+            popupContent.appendChild(popupPhoto);
+        }
+    }
+
+    
 });
